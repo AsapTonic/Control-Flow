7a87bc3a771973b7e15f54446e15229f3355d0ea
Antonio Rosario <antonio.rosario340@gmail.com>
2025-06-13 15:35:50 -0400

    SwitchCase

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-let glassFull= false;
+let day = "Tuesday";
 
-while(!glassFull){
-    console.log("pouring water...");
-    glassFull=true;
+switch (day){
+
+case "Monday":
+    console.log("Start of the Week");
+    break;
+
+    case "friday":
+        console.log("Almost weekend");
+        break;
+        case 'Saturay':
+            
+
+            case 'Sunday':
+                comsole.log('End of weekend');
+                break;
+                default:
+                    console.log ('Just Another Day');
 }
 
-
-let n= 5;
-
-while (n>0){
-console.log (n);
-n--;
-}
-
-console.log ("Blast Off");
